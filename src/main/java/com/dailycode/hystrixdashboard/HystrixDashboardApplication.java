27da2b21420e1ca3681d7ab3ad08153d83992a53
Huiying Wang <whymissu1228@gmail.com>
2022-11-09 11:40:00 -0800

    init and config http://localhost:9295/hystrix

---
+++ src/main/java/com/dailycode/hystrixdashboard/HystrixDashboardApplication.java
@@ -0,0 +1,18 @@
+package com.dailycode.hystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "application");
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+
+}
